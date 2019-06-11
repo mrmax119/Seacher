b4648d29a88dd4775e2f3bddc18f4b94a77fe733
trygetname <trygetmexchain@gmail.com>
2019-06-11 23:08:44 +0500

    v1

---
+++ Search/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace Search
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(15, 25);
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(551, 20);
+            this.maskedTextBox1.TabIndex = 0;
+            this.maskedTextBox1.TextChanged += new System.EventHandler(this.maskedTextBox1_TextChanged);
+            this.maskedTextBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.maskedTextBox1_KeyDown);
+            this.maskedTextBox1.KeyUp += new System.Windows.Forms.KeyEventHandler(this.maskedTextBox1_KeyUp);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(15, 52);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(632, 355);
+            this.listBox2.TabIndex = 4;
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(572, 23);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(659, 416);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Search/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Search
+{
+    public partial class Form1 : Form
+    {
+        const int countSearch = 101;
+        string[] dataSearch = new string[countSearch];
+        string[] dataSites = new string[countSearch];
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            #region search
+            dataSearch[0] = "maxim is the best";
+            dataSearch[1] = "photo";
+            dataSearch[2] = "english";
+            dataSearch[3] = "maximka";
+            dataSearch[4] = "10";
+            dataSearch[5] = "potato";
+            dataSearch[6] = "pomidor";
+            dataSearch[7] = "pomidor ???";
+            dataSearch[8] = "google";
+            dataSearch[9] = "yandex";
+            dataSearch[10] = "vk";
+            dataSearch[11] = "vkontakte";
+            dataSearch[12] = "SECRET";
+            #endregion
+
+            #region sites
+            dataSites[0] = "https://google.com";
+            dataSites[1] = "https://yandex.com";
+            dataSites[2] = "https://vk.com";
+            dataSites[3] = "https://google.com";
+            dataSites[4] = "https://vk.com";
+            dataSites[5] = "https://yandex.com";
+            dataSites[6] = "https://vk.com";
+            dataSites[7] = "https://google.com";
+            dataSites[8] = "https://google.com";
+            dataSites[9] = "https://yandex.com";
+            dataSites[12] = "https://youtube.com";
+            #endregion
+
+
+            for (int i = 13; i < countSearch; i++)
+            {
+                dataSearch[i] = Convert.ToString(i);
+            }
+
+            this.Text = Application.ProductName;
+        }
+
+        private void maskedTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        void Run()
+        { 
+            string str = maskedTextBox1.Text;
+            char[] c = str.ToCharArray();
+
+            for (int b = 0; b < countSearch; b++)
+            {
+                string str1 = string.Empty;
+                string str2 = string.Empty;
+                char[] strA5 = dataSearch[b].ToCharArray();
+
+                for (int j = 0; j < c.Length; j++)
+                {
+                    try
+                    {
+                        str1 += c[j];
+                        str2 += strA5[j];
+
+                        if (str1 == str2)
+                        {
+                            this.Invoke((MethodInvoker) delegate
+                            {
+                                listBox2.Items.Remove(dataSearch[b]);
+                                listBox2.Items.Add(dataSearch[b]);
+                            });
+                            
+                        }
+                        else
+                        {
+                            this.Invoke((MethodInvoker) delegate
+                            {
+                                listBox2.Items.Remove(dataSearch[b]);
+                            });
+                        }
+                    }
+                    catch { }
+                }
+            }
+            this.Invoke((MethodInvoker)delegate
+            {
+                button1.Enabled = true;
+            });
+
+        }
+
+        Thread thread;
+        bool bl = false;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!bl)
+            {
+                var s = sender as Button;
+                s.Enabled = false;
+                bl = false;
+            }
+            else
+            {
+                button1.Enabled = false;
+            }
+            
+            listBox2.Items.Clear();
+
+            thread = new Thread(Run);
+            thread.Start();
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            for (int i = 0; i < dataSearch.Length; i++)
+            {
+                if ((string)listBox2.SelectedItem == dataSearch[i])
+                {
+                    Process.Start(dataSites[i]);
+                    break;
+                }
+            }
+        }
+
+        private void maskedTextBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                bl = true;
+                button1_Click(null, null);
+            }
+        }
+
+        private void maskedTextBox1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Back && e.Control)
+            {
+                maskedTextBox1.Text = string.Empty;
+            }
+        }
+    }
+}
+
